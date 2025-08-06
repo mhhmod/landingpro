ac27bf6cafca65fdece216bf56412a343e3dad06
mhhmod <74679578+mhhmod@users.noreply.github.com>
2025-08-06 20:10:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch("https://ipapi.co/json/")
   .then(response => response.json())
   .then(data => {
     // استبدل "AE" برمز دولتك (مثلاً "EG" لمصر) إذا كنت تريد تغيير التقييد
-    if (data.country_code !== "AE") {
+    if (data.country_code !== "EG") {
       document.body.innerHTML = 
         "<h2 style='text-align:center;margin-top:50px;'>🚫 هذا المحتوى غير متاح في بلدك</h2>";
     }
