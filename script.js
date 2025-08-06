82487219a1f5b9798c4f39e9ac093a0e01f1992f
mhhmod <74679578+mhhmod@users.noreply.github.com>
2025-08-06 19:35:42 +0200

    1stcommit

---
+++ script.js
@@ -0,0 +1,11 @@
+// جلب بيانات الـ IP عبر خدمة ipapi.co
+fetch("https://ipapi.co/json/")
+  .then(response => response.json())
+  .then(data => {
+    // استبدل "AE" برمز دولتك (مثلاً "EG" لمصر) إذا كنت تريد تغيير التقييد
+    if (data.country_code !== "AE") {
+      document.body.innerHTML = 
+        "<h2 style='text-align:center;margin-top:50px;'>🚫 هذا المحتوى غير متاح في بلدك</h2>";
+    }
+  })
+  .catch(err => console.error(err));
